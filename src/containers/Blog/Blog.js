0ba7555ffe27d2f4b3d22c87f3a569456eddb30e
Jen-Po Cheng <jenpo.cheng.tw@gmail.com>
2018-02-25 23:09:48 -0500

    183. Absolute vs Relative Paths

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,6 +14,8 @@ class Blog extends Component {
                         <ul>
                             <li><Link to="/">Home</Link></li>
                             <li><Link to={{
+                                // Relative Path
+                                // pathname: {this.props.match.url + '/new-post'},
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
