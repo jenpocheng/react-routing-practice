de9840fcea345c91d8c4809d6167d20027d7c765
Jen-Po Cheng <jenpo.cheng.tw@gmail.com>
2018-02-25 19:06:53 -0500

    178. Rendering components for routes

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -34,16 +34,16 @@ class Posts extends Component {
   render () {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
     if (!this.state.error) {
-        posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />;
-        });
+      posts = this.state.posts.map(post => {
+        return <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)} />;
+      });
     }
     return (
-      <section classNmae="Posts">
+      <section className="Posts">
         {posts}
       </section>
     );
