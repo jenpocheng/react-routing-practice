ffbd6be720aec30f87938c3aed3c718ff960ab50
Jen-Po Cheng <jenpo.cheng.tw@gmail.com>
2018-02-25 19:37:14 -0500

    181. Using routing-related props

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -8,7 +8,9 @@ class Posts extends Component {
   state = {
     posts: []
   }
+
   componentDidMount () {
+    console.log(this.props);
     axios.get( '/posts' )
         .then( response => {
           const posts = response.data.slice(0, 4);
